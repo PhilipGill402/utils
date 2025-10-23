e9b37a72b7be4d06f6855a43dcdeeacfda581efb
FurryBasket9978 <pgillbama@icloud.com>
2025-10-22 19:23:10 -0500

    i don't know

--- tests/test.c
+++ tests/test.c
@@ -8,9 +8,7 @@ int main() {
         enqueue(queue, val);
     }
     
-    for (int i = 0; i < 10; i++){
-        dequeue(queue);
-    }
+    
     print_queue(queue);
     value_t val = {.type = VAL_CHAR, .val.c = 'A'};
 
