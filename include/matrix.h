a3912bbb1ba1e94dab7a97c237de49a58d6e68ef
FurryBasket9978 <pgillbama@icloud.com>
2025-11-10 17:51:08 -0600

    added math functions to implement later to the matrix header file

--- include/matrix.h
+++ include/matrix.h
@@ -17,6 +17,19 @@ void matrix_release(matrix_t* matrix);
 void matrix_set(matrix_t* matrix, int row, int col, double num);
 double matrix_get(const matrix_t* matrix, int row, int col);
 
+//math
+matrix_t matrix_add(const matrix_t* matrix_a, const matrix_t* matrix_b);
+matrix_t matrix_sub(const matrix_t* matrix_a, const matrix_t* matrix_b);
+matrix_t matrix_scalar_mul(const matrix_t* matrix, double scalar);
+matrix_t matrix_mul(const matrix_t* matrix_a, const matrix_t* matrix_b);
+matrix_t matrix_transpose(const matrix_t* matrix);
+
+//equality
+int matrix_equal(const matrix_t* a, const matrix_t* b, double tol);
+int matrix_is_square(const matrix_t* m);
+int matrix_is_symmetric(const matrix_t* m, double tol);
+int matrix_is_identity(const matrix_t* m, double tol);
+
 //miscellaneous
 void matrix_print(const matrix_t* matrix);
 matrix_t matrix_copy(const matrix_t* matrix);
